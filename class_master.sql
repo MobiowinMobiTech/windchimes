877878c5c12ec5812054ba23f862ab411a0ebfe9
mobiOwin <raman31oct@gmail.com>
2017-04-25 19:43:46 +0530

    Add files via upload
    Class master insert

---
+++ class_master.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM windchimp.class_master
+LIMIT 0, 1000
+
+-- Date: 2017-04-25 19:42
+*/
+INSERT INTO `class_master` (`id`,`class_id`,`class_name`,`discription`,`created_by`,`created_dt`,`modified_by`,`modified_dt`,`del_flag`) VALUES (1,'1','KG','KG','SYSTEM',NULL,'SYSTEM',NULL,'F');
+INSERT INTO `class_master` (`id`,`class_id`,`class_name`,`discription`,`created_by`,`created_dt`,`modified_by`,`modified_dt`,`del_flag`) VALUES (2,'2','TOD','TOD','SYSTEM',NULL,'SYSTEM',NULL,'F');
+INSERT INTO `class_master` (`id`,`class_id`,`class_name`,`discription`,`created_by`,`created_dt`,`modified_by`,`modified_dt`,`del_flag`) VALUES (3,'3','PG','PG','SYSTEM',NULL,'SYSTEM',NULL,'F');
+INSERT INTO `class_master` (`id`,`class_id`,`class_name`,`discription`,`created_by`,`created_dt`,`modified_by`,`modified_dt`,`del_flag`) VALUES (4,'4','PN','PN','SYSTEM',NULL,'SYSTEM',NULL,'F');
+INSERT INTO `class_master` (`id`,`class_id`,`class_name`,`discription`,`created_by`,`created_dt`,`modified_by`,`modified_dt`,`del_flag`) VALUES (5,'5','NUR','NUR','SYSTEM',NULL,'SYSTEM',NULL,'F');
